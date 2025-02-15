4718a0166fb12d6951d93b73c84c55fa21a869dd
rzhbishal <bishal_khadka@reazon.jp>
2025-02-16 00:11:31 +0900

    feat: currency added

--- src/app/onboarding/layout.tsx
+++ src/app/onboarding/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative flex h-screen w-full flex-col items-center justify-center">
+    <div className="relative flex h-screen w-full flex-col items-center justify-center p-5">
       {children}
     </div>
   );
