651f04b069f941a89af07159c2e454d5b3cf0383
ghbishal <bishalk2056@gmail.com>
2025-04-09 03:33:52 +0900

    feat: tanstack query

--- src/provider/RootProviders.tsx
+++ src/provider/RootProviders.tsx
@@ -1,15 +1,23 @@
+"use client";
+
 import { ReactNode } from "react";
 import { ThemeProvider } from "next-themes";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export function RootProviders({ children }: { children: ReactNode }) {
+  const queryClient = new QueryClient();
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
